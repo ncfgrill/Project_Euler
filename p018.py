d2e74c54b1c4d3fd5e70d742c22f7af3193c1589
Nathaniel Grill <65606252+ncfgrill@users.noreply.github.com>
2021-05-10 18:39:11 -0500

    Update p018.py

--- p018.py
+++ p018.py
@@ -30,21 +30,18 @@ tri = [[75],
 sum_tri = deepcopy(tri)
 
 for l in range(len(sum_tri) - 1):
-  for r in range(len(sum_tri[l])):
-    sum_tri[l][r] = 0
+    for r in range(len(sum_tri[l])): sum_tri[l][r] = 0
 
 l = len(sum_tri) - 1
 while l > 0:
-  for r in range(len(sum_tri[l])):
-    if r < len(sum_tri[l]) - 1:
-      s = sum_tri[l][r] + tri[l-1][r]
-      if s > sum_tri[l-1][r]:
-        sum_tri[l-1][r] = s
-    if r > 0:
-      s = sum_tri[l][r] + tri[l-1][r-1]
-      if s > sum_tri[l-1][r-1]:
-        sum_tri[l-1][r-1] = s
+    for r in range(len(sum_tri[l])):
+        if r < len(sum_tri[l]) - 1:
+            s = sum_tri[l][r] + tri[l-1][r]
+            if s > sum_tri[l-1][r]: sum_tri[l-1][r] = s
+        if r > 0:
+            s = sum_tri[l][r] + tri[l-1][r-1]
+            if s > sum_tri[l-1][r-1]: sum_tri[l-1][r-1] = s
 
-  l -= 1
+    l -= 1
 
 print('Maximum total:', sum_tri[0][0])
