08924dd75d84d00e9b446d63a7a649fed935afb1
Nathaniel Grill <65606252+ncfgrill@users.noreply.github.com>
2021-05-10 17:05:03 -0500

    Update p003.py

--- p003.py
+++ p003.py
@@ -9,33 +9,28 @@
 #                              Author: ncfgrill                                #
 ################################################################################
 
-import math
+from math import ceil, sqrt
 
 def check_prime(num):
-  if num <= 3:
-    return num > 1
-  if num % 6 != 1 and num % 6 != 5:
-    return False
-  for i in range(2, math.ceil(math.sqrt(num)) + 1):
-    if num % i == 0:
-      return False
+    if num <= 3:                        return num > 1
+    elif num % 6 != 1 and num % 6 != 5: return False
+    
+    for i in range(2, math.ceil(math.sqrt(num)) + 1):
+        if num % i == 0: return False
 
   return True
 
 def get_factor():
-  d = 600851475143
-  factors = []
-  p = 3
-  while d != 1:
-    if check_prime(p):
-      while True:
-        if d % p != 0:
-          break
-        d /= p
-        if p not in factors:
-          factors.append(p)
-    p += 2
+    d, factor, p = 600851475143, 1, 3
+  
+    while d != 1:
+        if check_prime(p):
+            while True:
+                if d % p != 0: break
+                d /= p
+                factor = p
+        p += 2
 
-  return factors[-1]
+  return factor
 
 print('Largest prime factor:', get_factor())
