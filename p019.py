e85f3c4d47a89cf60cb539a2359b274557852eff
Nathaniel Grill <65606252+ncfgrill@users.noreply.github.com>
2021-05-10 18:43:26 -0500

    Update p019.py

--- p019.py
+++ p019.py
@@ -16,24 +16,18 @@ days31 = [1, 3, 5, 7, 8, 10, 12]
 days30 = [4, 6, 9, 11]
 
 while year < 2001:
-  if day == 0:
-    total += 1
+    if day == 0: total += 1
 
-  if month in days31:
-    day += 31
-  elif month in days30:
-    day += 30
-  else: # February...
-    if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
-      day += 29
-    else:
-      day += 28
+    if month in days31:   day += 31
+    elif month in days30: day += 30
+    else: # February...
+        if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0): day += 29
+        else:                                                      day += 28
 
-  if month == 12:
-    month = 1
-    year += 1
-  else:
-    month += 1
-  day %= 7
+    if month == 12:
+        month = 1
+        year += 1
+    else: month += 1
+    day %= 7
 
 print('Sundays:', total)
