2649333db2bfe96a87ae68f8164a5e36c5b22712
Nathaniel Grill <65606252+ncfgrill@users.noreply.github.com>
2021-05-10 17:58:48 -0500

    Update p010.py

--- p010.py
+++ p010.py
@@ -9,28 +9,24 @@
 #                              Author: ncfgrill                                #
 ################################################################################
 
-import math
+from math import ceil, sqrt
 
 def check_prime(num):
-  if num <= 3:
-    return num > 1
-  if num % 6 != 1 and num % 6 != 5:
-    return False
-  for i in range(2, math.ceil(math.sqrt(num)) + 1):
-    if num % i == 0:
-      return False
+    if num <= 3:                        return num > 1
+    elif num % 6 != 1 and num % 6 != 5: return False
+    
+    for i in range(2, ceil(sqrt(num)) + 1):
+        if num % i == 0: return False
 
-  return True
+    return True
 
 def sum_primes():
-  s = 2
-  p = 3
+    s, p = 2, 3
 
-  while p < 2000000:
-    if check_prime(p):
-      s += p
-    p += 2
+    while p < 2000000:
+        if check_prime(p): s += p
+        p += 2
 
-  return s
+    return s
 
 print('Sum:', sum_primes())
