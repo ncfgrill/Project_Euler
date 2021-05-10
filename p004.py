d0593deff38de69ac57be0aefd5410b74b7af916
Nathaniel Grill <65606252+ncfgrill@users.noreply.github.com>
2021-05-10 17:13:30 -0500

    Update p004.py

--- p004.py
+++ p004.py
@@ -10,25 +10,23 @@
 ################################################################################
 
 def check_if_pal(num):
-  l = len(num)
-  for i in range(l // 2):
-    if num[i] != num[l - i - 1]:
-      return False
+    l = len(num)
+    for i in range(l // 2):
+        if num[i] != num[l - i - 1]: return False
 
-  return True
+    return True
 
 def find_pal():
-  i, hi = 100, 0
+    i, hi = 100, 0
 
-  while i < 1000:
-    j = 100
-    while j < 1000:
-      p = i * j
-      if p > hi and check_if_pal(str(p)):
-        hi = p
-      j += 1
-    i += 1
+    while i < 1000:
+        j = 100
+        while j < 1000:
+            p = i * j
+            if p > hi and check_if_pal(str(p)): hi = p
+            j += 1
+        i += 1
 
-  return hi
+    return hi
 
 print('Largest palindrome:', find_pal())
