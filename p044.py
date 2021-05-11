117f0235f8a0470c4aeb4b9199cef88d33551cab
Nathaniel Grill <65606252+ncfgrill@users.noreply.github.com>
2021-05-10 23:09:03 -0500

    Update p044.py

--- p044.py
+++ p044.py
@@ -13,28 +13,12 @@
 
 from math import inf
 
-pent_set = set()
-pent_list = []
+pents = {n : (n * (3*n - 1)) // 2 for n in range(1, 2501)}
+pents_set, dist = set(pents), inf
 
-def calc_pents():
-  n = 1
-  while n <= 2500:
-    p = (n * (3*n - 1)) // 2
-    pent_set.add(p)
-    pent_list.append(p)
-    n += 1
+for j in range(1, len(pents) - 1):
+    for k in range(j+1, len(pents)):
+        s, d = pents[k] + pents[j], pents[k] - pents[j]
+        if s in pents_set and d in pents_set and d < dist: dist = d
 
-def minimize():
-  global pent_set, pent_list
-  dist = inf
-
-  for j in range(len(pent_list) - 2):
-    for k in range(j+1, len(pent_list) - 1):
-      s, d = pent_list[k] + pent_list[j], pent_list[k] - pent_list[j]
-      if s in pent_set and d in pent_set and d < dist:
-        dist = d
-
-  return dist
-
-calc_pents()
-print('D:', minimize())
+print('D:', dist)
