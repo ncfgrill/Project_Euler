d0708a61c2a4ed5f4000f2dc9321105ac8ac5761
Nathaniel Grill <65606252+ncfgrill@users.noreply.github.com>
2021-05-10 22:36:54 -0500

    Update p039.py

--- p039.py
+++ p039.py
@@ -11,23 +11,20 @@
 
 from math import sqrt
 
-perimeters = {}
-a = 1
+perimeters, a = {}, 1
 
 while 2*a + sqrt(2 * a**2) <= 1000:
-  b = a
-  while True:
-    c = sqrt(a**2 + b**2)
-    p = a + b + c
-    if p > 1000:
-      break
-    if c % 1 == 0:
-      if p in perimeters:
-        perimeters[p] = perimeters[p] + 1
-      else:
-        perimeters[p] = 1
-    b += 1
-  a += 1
+    b = a
+    while True:
+        c = sqrt(a**2 + b**2)
+        p = a + b + c
+        
+        if p > 1000: break
+        if c % 1 == 0:
+            if p in perimeters: perimeters[p] = perimeters[p] + 1
+            else:               perimeters[p] = 1
+        b += 1
+    a += 1
 
 perimeters = sorted(perimeters.items(), key=lambda kv: kv[1], reverse=True)
 print('Perimeter:', perimeters[0][0])
