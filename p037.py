f6e7591583bfeede2dbb8f97667613dfa0fb9873
Nathaniel Grill <65606252+ncfgrill@users.noreply.github.com>
2021-05-10 22:28:01 -0500

    Update p037.py

--- p037.py
+++ p037.py
@@ -13,44 +13,41 @@
 from math import ceil, sqrt
 
 def check_prime(num):
-  if num <= 3:
-    return num > 1
-  if num % 6 != 1 and num % 6 != 5:
-    return False
-  for i in range(2, ceil(sqrt(num)) + 1):
-    if num % i == 0:
-      return False
+    if num <= 3:                        return num > 1
+    elif num % 6 != 1 and num % 6 != 5: return False
+  
+    for i in range(2, ceil(sqrt(num)) + 1):
+        if num % i == 0: return False
 
-  return True
+    return True
 
 def truncate(num):
-  save = num
-  #left-to-right
-  while len(num) > 1:
-    num = num[1:]
-    if not check_prime(int(num)):
-      return False
+    save = num
+  
+    #left-to-right
+    while len(num) > 1:
+        num = num[1:]
+        if not check_prime(int(num)): return False
 
-  num = save
+    num = save
 
-  # right-to-left
-  while len(num) > 1:
-    num = num[0:-1]
-    if not check_prime(int(num)):
-      return False
+    # right-to-left
+    while len(num) > 1:
+        num = num[0:-1]
+        if not check_prime(int(num)): return False
 
-  return True
+    return True
 
 def find_primes():
-  primes, d, s = 0, 11, 0
-  while primes < 11:
-    if check_prime(d):
-      if truncate(str(d)):
-        primes += 1
-        s += d
+    primes, d, s = 0, 11, 0
+    while primes < 11:
+        if check_prime(d):
+            if truncate(str(d)):
+                primes += 1
+                s += d
 
-    d += 2
+        d += 2
 
-  return s
+    return s
 
 print('Sum:', find_primes())
