92c4d448e03a154ca8021bdab5452e78e164f74a
Nathaniel Grill <65606252+ncfgrill@users.noreply.github.com>
2021-05-10 21:24:36 -0500

    Update p031.py

--- p031.py
+++ p031.py
@@ -14,16 +14,14 @@ coins = [200, 100, 50, 20, 10, 5, 2, 1]
 combs = 0
 
 def combinations(value, c):
-  global combs
-  if value != 0:
-    if value < coins[c]:
-      combinations(value, c+1)
-    else:
-      combinations(value - coins[c], c)
-      if c + 1 < len(coins):
-        combinations(value, c+1)
-  else:
-    combs += 1
+    global combs
+    if value != 0:
+        if value < coins[c]: combinations(value, c+1)
+        else:
+            combinations(value - coins[c], c)
+            if c + 1 < len(coins): combinations(value, c+1)
+    
+    else: combs += 1
 
 combinations(200, 0)
 print('Combinations:', combs)
