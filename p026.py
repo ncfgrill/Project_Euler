9aa778a82b1266ba20adb846475172cabbf7bdde
Nathaniel Grill <65606252+ncfgrill@users.noreply.github.com>
2021-05-10 20:50:46 -0500

    Update p026.py

--- p026.py
+++ p026.py
@@ -10,24 +10,20 @@
 #                              Author: ncfgrill                                #
 ################################################################################
 
-d = 2
-save_diff, save_d = 0, 0
+d, save_diff, save_d = 2, 0, 0
 
 while d < 1000:
-  l = {}
-  i = 0
-  while True:
-    m = (10 ** i) % d
-    if m not in l:
-      l[m] = i
-      i += 1
-      continue
-    diff = i - l[m]
-    break
+    l, i = {}, 0
+    while True:
+        m = (10 ** i) % d
+        if m not in l:
+            l[m] = i
+            i += 1
+            continue
+        diff = i - l[m]
+        break
 
-  if diff > save_diff:
-    save_diff, save_d = diff, d
-
-  d += 1
+    if diff > save_diff: save_diff, save_d = diff, d
+    d += 1
 
 print('d:', save_d)
