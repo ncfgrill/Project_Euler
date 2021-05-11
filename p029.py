5246ccbe6e33553c372805c7228d3db08ec038a8
Nathaniel Grill <65606252+ncfgrill@users.noreply.github.com>
2021-05-11 13:30:38 -0500

    Update p029.py

--- p029.py
+++ p029.py
@@ -10,10 +10,4 @@
 #                              Author: ncfgrill                                #
 ################################################################################
 
-terms = set()
-
-for a in range(2, 101):
-    for b in range(2, 101):
-        terms.add(a**b)
-
-print('Terms:', len(terms))
+print('Terms:', len({a**b for a in range(2,101) for b in range(2,101)}))
