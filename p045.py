093446927ebeac7a20f16a433846d175313e89ee
Nathaniel Grill <65606252+ncfgrill@users.noreply.github.com>
2021-05-10 23:12:16 -0500

    Update p045.py

--- p045.py
+++ p045.py
@@ -11,34 +11,33 @@
 ################################################################################
 
 def get_tria(num):
-  return num * (num + 1) // 2
+    return num * (num + 1) // 2
 
 def get_pent(num):
-  return num * (3 * num - 1) // 2
+    return num * (3 * num - 1) // 2
 
 def get_hexa(num):
-  return num * (2 * num - 1)
+    return num * (2 * num - 1)
 
 def find_number():
-  t, p, h = 286, 165, 143
-  tria, pent, hexa = get_tria(t), get_pent(p), get_hexa(h)
+    t, p, h = 286, 165, 143
+    tria, pent, hexa = get_tria(t), get_pent(p), get_hexa(h)
 
-  while True:
-    if tria == pent and tria == hexa:
-      break
+    while True:
+        if tria == pent and tria == hexa: break
 
-    while tria < pent or tria < hexa:
-      t += 1
-      tria = get_tria(t)
+        while tria < pent or tria < hexa:
+            t += 1
+            tria = get_tria(t)
 
-    while pent < tria:
-      p += 1
-      pent = get_pent(p)
+        while pent < tria:
+            p += 1
+            pent = get_pent(p)
 
-    while hexa < tria:
-      h += 1
-      hexa = get_hexa(h)
+        while hexa < tria:
+            h += 1
+            hexa = get_hexa(h)
 
-  return tria
+    return tria
 
 print('Number:', find_number())
