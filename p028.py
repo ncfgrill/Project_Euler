72534ff19a5a89c5dfbf035d6667b88887dba8ff
Nathaniel Grill <65606252+ncfgrill@users.noreply.github.com>
2021-05-11 13:23:39 -0500

    Update p028.py

--- p028.py
+++ p028.py
@@ -9,10 +9,12 @@
 #                              Author: ncfgrill                                #
 ################################################################################
 
-s, c = 1, 2
+c = 0
 
-for n in range(3, 1003, 2):
-    s += n**2 + (n**2 - c) + ((n-1)**2 + 1) + (n**2 - c*3)
+def sum_diag(n):
+    global c
+
     c += 2
+    return n**2 + (n**2 - c) + ((n-1)**2 + 1) + (n**2 - c*3)
 
-print('Sum:', s)
+print('Sum:', 1 + sum(map(sum_diag, [n for n in range(3,1003,2)])))
