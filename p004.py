116ea40bd5834b92a814e4387904fc4d71bf9212
Nathaniel Grill <65606252+ncfgrill@users.noreply.github.com>
2021-05-11 11:24:00 -0500

    Update p004.py

--- p004.py
+++ p004.py
@@ -9,20 +9,14 @@
 #                              Author: ncfgrill                                #
 ################################################################################
 
-def check_palindrome(num):
-    return num == num[::-1]
+i, hi = 100, 0
 
-def find_pal():
-    i, hi = 100, 0
+while i < 1000:
+    j = 100
+    while j < 1000:
+        p = i * j
+        if p > hi and str(p) == str(p)[::-1]: hi = p
+        j += 1
+    i += 1
 
-    while i < 1000:
-        j = 100
-        while j < 1000:
-            p = i * j
-            if p > hi and check_palindrome(str(p)): hi = p
-            j += 1
-        i += 1
-
-    return hi
-
-print('Largest palindrome:', find_pal())
+print('Largest palindrome:', hi)
