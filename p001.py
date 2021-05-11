0ef4923f801b76c4f8619d754ef147f36c50f1d8
Nathaniel Grill <65606252+ncfgrill@users.noreply.github.com>
2021-05-11 11:15:51 -0500

    Update p001.py

--- p001.py
+++ p001.py
@@ -9,4 +9,4 @@
 #                              Author: ncfgrill                                #
 ################################################################################
 
-print('Sum:', sum([x for x in range(3, 1000) if x % 3 == 0 or x % 5 == 0]))
+print('Sum:', sum(x for x in range(3, 1000) if x % 3 == 0 or x % 5 == 0))
