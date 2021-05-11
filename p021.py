1e2eb8bda4324d3e1516edc76db3ea3cd691402f
Nathaniel Grill <65606252+ncfgrill@users.noreply.github.com>
2021-05-10 19:46:41 -0500

    Update p021.py

--- p021.py
+++ p021.py
@@ -12,40 +12,32 @@
 from math import ceil, sqrt
 
 def find_factors_sum(num):
-  factors = [1]
+    factors = [1]
 
-  for f1 in range(2, ceil(sqrt(num)) + 1):
-    if num % f1 == 0:
-      f2 = num // f1
-      if f1 not in factors:
-        factors.append(f1)
-      if f2 not in factors:
-        factors.append(f2)
-  if num in factors:
-    factors.remove(num)
-  return sum(factors)
+    for f1 in range(2, ceil(sqrt(num)) + 1):
+        if num % f1 == 0:
+            f2 = num // f1
+            if f1 not in factors: factors.append(f1)
+            if f2 not in factors: factors.append(f2)
+    if num in factors: factors.remove(num)
+    return sum(factors)
 
 def find_amicable():
-  n = 2
-  f_sum = {1:0}
-  while n < 10000:
-    f_sum[n] = find_factors_sum(n)
-    n += 1
+    n, f_sum = 2, {1:0}
+    while n < 10000:
+        f_sum[n] = find_factors_sum(n)
+        n += 1
 
-  amicable = set()
-  s = 0
-  for n1 in range(2, 10000):
-    if n1 in amicable:
-      continue
-    n2 = f_sum[n1]
-    if n1 == n2:
-      continue
-    if n2 < 10000 and n2 not in amicable:
-      if f_sum[n2] == n1:
-        amicable.add(n1)
-        amicable.add(n2)
-        s = s + n1 + n2
-
-  return s
+    amicable, s = set(), 0
+    for n1 in range(2, 10000):
+        if n1 in amicable: continue
+        n2 = f_sum[n1]
+        if n1 == n2: continue
+        if n2 < 10000 and n2 not in amicable:
+            if f_sum[n2] == n1:
+                amicable.add(n1)
+                amicable.add(n2)
+                s += n1 + n2
+    return s
 
 print('Sum:', find_amicable())
