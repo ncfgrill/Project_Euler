f0732d9e5cc4d5d39f5683471fe1e612ac255496
Nathaniel Grill <65606252+ncfgrill@users.noreply.github.com>
2021-05-10 21:36:25 -0500

    Update p033.py

--- p033.py
+++ p033.py
@@ -13,28 +13,19 @@
 fractions = []
 
 for n in range(10, 100):
-  for d in range(n+1, 100):
-    if n % 10 == 0 and d % 10 == 0:
-      continue
-    save = n / d
+    for d in range(n+1, 100):
+        if n % 10 == 0 and d % 10 == 0: continue
+        save, a, b = n / d, str(n), str(d)
+        
+        if a[0] == b[0]:   a, b = int(a[1]), int(b[1])
+        elif a[0] == b[1]: a, b = int(a[1]), int(b[0])
+        elif a[1] == b[0]: a, b = int(a[0]), int(b[1])
+        elif a[1] == b[1]: a, b = int(a[0]), int(b[0])
+        else:              continue
 
-    a, b = str(n), str(d)
-    if a[0] == b[0]:
-      a, b = int(a[1]), int(b[1])
-    elif a[0] == b[1]:
-      a, b = int(a[1]), int(b[0])
-    elif a[1] == b[0]:
-      a, b = int(a[0]), int(b[1])
-    elif a[1] == b[1]:
-      a, b = int(a[0]), int(b[0])
-    else:
-      continue
-
-    if b != 0 and save == a / b:
-      fractions.append((n, d))
+        if b != 0 and save == a / b: fractions.append((n, d))
 
 d = 1
-for f in fractions:
-  d *= (f[1] / f[0])
+for f in fractions: d *= f[1] / f[0]
 
 print('Denominator:', d)
