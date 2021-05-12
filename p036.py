e5826d1fd579c64e9252ebd8c964ed061c19ab44
Nathaniel Grill <65606252+ncfgrill@users.noreply.github.com>
2021-05-11 20:03:45 -0500

    Update p036.py

--- p036.py
+++ p036.py
@@ -10,18 +10,9 @@
 #                              Author: ncfgrill                                #
 ################################################################################
 
-def to_binary(num):
-    b = ''
-    while num > 0:
-        if num % 2 == 0: b = '0' + b
-        else:            b = '1' + b
-        num //= 2
-
-    return b
-
-def find_palindromes(num):
-    binary = to_binary(num)
-    return num if str(num) == str(num)[::-1] and binary == binary[::-1] else 0
+def find_palindromes(n):
+    b = bin(n)
+    return n if str(n) == str(n)[::-1] and b[2:] == b[:1:-1] else 0
 
 # Only check odd numbers as even numbers always begin with 1 and end with 0
 print('Sum:', sum(map(find_palindromes, [x for x in range(1,1000000,2)])))
