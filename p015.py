b682dc35ec0c9c4acf6c14ff4be8778cf1d98d09
Nathaniel Grill <65606252+ncfgrill@users.noreply.github.com>
2021-05-19 12:14:35 -0500

    Update p015.py

--- p015.py
+++ p015.py
@@ -15,9 +15,6 @@ from math import prod
 # d choose n
 d, n = 40, 20
 
-# d!
-c = prod(i for i in range(1, d+1))
-# n!(d-n)!
-c //= (prod(j for j in range(1, n+1)) * prod(k for k in range(1, d-n+1)))
+c = prod(i for i in range(d-n+1, d+1)) // prod(j for j in range(1, n+1))
 
 print('Routes:', c)
